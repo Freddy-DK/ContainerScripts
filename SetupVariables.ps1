e0f96d673691f3a456eab33c1edce6560c9e31da
Freddy Kristiansen <fkr@freddy.dk>
2026-03-16 13:41:28 +0100

    default licensefile

--- SetupVariables.ps1
+++ SetupVariables.ps1
@@ -1,2 +1,5 @@
 $ENV:DatabaseInstance = ''
+if ("$env:licensefile" -eq "") {
+    $ENV:licensefile = @(Get-Item "C:\Program Files\Microsoft Dynamics NAV\*\Service\*.bclicense")[0].FullName
+}
 . c:\run\SetupVariables.ps1
