c823d948106b0c7ab2d26cec5d5f793dcf44d784
Freddy Kristiansen <fkr@freddy.dk>
2026-04-21 13:46:44 +0200

    add sync

--- SetupTenant.ps1
+++ SetupTenant.ps1
@@ -1,4 +1,6 @@
-$tenantDatabaseName = "$($ENV:DatabaseName)-default"
+$tenantId = 'default'
+$tenantDatabaseName = "$($ENV:DatabaseName)-$tenantId"
+
 $CustomConfigFile =  Join-Path $ServiceTierFolder "CustomSettings.config"
 $CustomConfig = [xml](Get-Content $CustomConfigFile)
 $tenantEnvironmentType = $customConfig.SelectSingleNode("//appSettings/add[@key='TenantEnvironmentType']")
@@ -6,10 +8,15 @@ $DatabaseServerInstance = "$DatabaseServer"
 if ("$DatabaseInstance" -ne "") {
     $DatabaseServerInstance += "\$DatabaseInstance"
 }
-Write-Host "Mounting tenant 'default' with database '$tenantDatabaseName' on database server instance '$DatabaseServerInstance'... on '$ServerInstance'" -ForegroundColor Green
-
 $Params = @{ "Force"=$true; "AllowAppDatabaseWrite" = $false; "OverwriteTenantIdInDatabase" = $true }
 if ($tenantEnvironmentType -ne $null) {
     $Params += @{"EnvironmentType" = $tenantEnvironmentType.value }
 }
-Mount-NavTenant -ServerInstance $ServerInstance -Tenant 'default' -DatabaseName $tenantDatabaseName -DatabaseCredentials $databaseCredentials @Params -WarningAction SilentlyContinue
+
+Write-Host "Mounting tenant '$tenantId' with database '$($DatabaseServerInstance):$tenantDatabaseName' on '$ServerInstance'"
+Mount-NavTenant -ServerInstance $ServerInstance -Tenant $tenantId -DatabaseName $tenantDatabaseName -DatabaseCredentials $databaseCredentials @Params -WarningAction SilentlyContinue
+
+Write-Host "Sync'ing Tenant"    
+Sync-NAVTenant  -ServerInstance $ServerInstance `
+                -Tenant $tenantId `
+                -Force
