967382270e467a2d5bc437c7c05dff6c8ab14715
Freddy Kristiansen <fkr@freddy.dk>
2026-04-21 12:37:46 +0200

    space

--- SetupTenant.ps1
+++ SetupTenant.ps1
@@ -7,4 +7,4 @@ $Params = @{ "Force"=$true; "AllowAppDatabaseWrite" = $false }
 if ($tenantEnvironmentType -ne $null) {
     $Params += @{"EnvironmentType" = $tenantEnvironmentType.value }
 }
-Mount-NavTenant-ServerInstance $ServerInstance -TenantId 'default' -DatabaseName $tenantDatabaseName @Params -WarningAction SilentlyContinue
+Mount-NavTenant -ServerInstance $ServerInstance -TenantId 'default' -DatabaseName $tenantDatabaseName @Params -WarningAction SilentlyContinue
