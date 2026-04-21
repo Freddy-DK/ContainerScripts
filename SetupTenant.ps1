3e1ddc7fc0c975f295547be7ee16252109845558
Freddy Kristiansen <fkr@freddy.dk>
2026-04-21 06:55:46 +0200

    Add SetupTenant

---
+++ SetupTenant.ps1
@@ -0,0 +1,10 @@
+$tenantDatabaseName = "$($ENV:DatabaseName)-default"
+$CustomConfigFile =  Join-Path $ServiceTierFolder "CustomSettings.config"
+$CustomConfig = [xml](Get-Content $CustomConfigFile)
+$tenantEnvironmentType = $customConfig.SelectSingleNode("//appSettings/add[@key='TenantEnvironmentType']")
+
+$Params = @{ "Force"=$true; "AllowAppDatabaseWrite" = $false }
+if ($tenantEnvironmentType -ne $null) {
+    $Params += @{"EnvironmentType" = $tenantEnvironmentType.value }
+}
+Mount-NavDatabase -ServerInstance $ServerInstance -TenantId 'default' -DatabaseName $tenantDatabaseName @Params -WarningAction SilentlyContinue
