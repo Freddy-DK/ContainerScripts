c35e2217da91e440668094d726336bbd89a528c8
Freddy Kristiansen <fkr@freddy.dk>
2026-04-21 12:33:41 +0200

    use mount-navtenant

--- SetupLicense.ps1
+++
@@ -1,6 +0,0 @@
-Write-Host "----------------------------------------------------------"
-$CustomConfigFile =  Join-Path $ServiceTierFolder "CustomSettings.config"
-Get-Content $CustomConfigFile | Out-Host
-Write-Host "----------------------------------------------------------"
-
-. c:\run\SetupLicense.ps1
